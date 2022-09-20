8c8aeeb3a77a43e66847b6e77e00da7f51202e5c
akashkad <akashkad@cybage.com>
2022-09-20 10:30:52 +0530

    ioffice Calender Rest API

---
+++ IofficeCalenderAPI/src/main/java/com/example/demo/IofficeCalenderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IofficeCalenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IofficeCalenderApiApplication.class, args);
+	}
+
+}
