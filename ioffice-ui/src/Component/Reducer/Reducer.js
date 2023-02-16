1bd052610310f5b0ddd1f24cbfd0ff342cf89aef
Rajani <rajanisa@cybage.com>
2023-02-16 18:04:15 +0530

    product and order testcases added

--- ioffice-ui/src/Component/Reducer/Reducer.js
+++ ioffice-ui/src/Component/Reducer/Reducer.js
@@ -1,11 +1,11 @@
 const Reducer = (cart = [], action) => {
     switch (action.type) {
         case 'ADD':
-            let addToCart = cart.filter((element) => (element.productId === action.payload.productId || element.product.productId !== action.payload.product.productId))
-            if (addToCart < 1) {
-                return [...cart, action.payload]
-            } else {
-                return cart;
+            let addToCart=cart.filter((element)=>(element.productId===action.payload.productId))
+        if(addToCart<1){
+            return [...cart,action.payload]
+        }else{
+            return cart;
             }
 
         case 'REMOVE':
