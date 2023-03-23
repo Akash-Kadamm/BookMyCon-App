38cc87e4509bca37d98be51f9c4e5bdab2519ec0
Rupali <rupalimat@cybage.com>
2023-03-23 14:12:42 +0530

    Files added

---
+++ BookMyConAPI/src/main/java/com/bookmycon/model/User.java
@@ -0,0 +1,36 @@
+package com.bookmycon.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_role")
+    private String userRole;
+
+    @Column(name = "user_contact")
+    private String userContact;
+
+    private String thumbnail;
+}
