308f1e575f2bc2949788fda2b0f568da89c8eefd
ameyd96 <erameydeshpande@gmail.com>
2023-04-10 10:22:52 +0530

    Ticket Id : 9AHqlS0Z : Junit Test cases
    ## What : Add junit test cases to controller and service layer
    
    ## Why : As per ticket description  Added junit test cases to controller and service layer

--- BookMyConAPI/src/test/java/com/bookmycon/controller/GuestControllerTest.java
+++ BookMyConAPI/src/test/java/com/bookmycon/controller/GuestControllerTest.java
@@ -75,82 +75,8 @@ public class GuestControllerTest {
         Assert.assertEquals(response, actual.getBody());
     }
 
-//    @Test
-//    public void testAddGuest() throws Exception {
-//        // create a GuestRequestDTO object with some data
-//        GuestRequestDTO guestRequestDTO = new GuestRequestDTO();
-//        guestRequestDTO.setGuestName("John Doe");
-//        guestRequestDTO.setGuestEmail("johndoe@example.com");
-//        guestRequestDTO.setThumbnail("thumbnail.png");
-//
-//        // create a Guest object to return from the guestService.save() method
-//        Guest newGuest = new Guest();
-//        newGuest.setGuestId(1L);
-//        newGuest.setGuestName("John Doe");
-//        newGuest.setGuestEmail("johndoe@example.com");
-//        newGuest.setThumbnail("thumbnail.png");
-//
-//        // use Mockito to mock the guestService.save() method
-//        when(guestService.save(any(Guest.class), anyString())).thenReturn(newGuest);
-//
-//        // call the addGuest() method of the guestController
-//        ResponseEntity<Guest> responseEntity = guestController.addGuest(guestRequestDTO);
-//
-//        // verify that the guestService.save() method was called with the correct parameters
-//        verify(guestService).save(argThat(guest -> guest.getGuestName().equals("John Doe") && guest.getGuestEmail().equals("johndoe@example.com")), eq("thumbnail.png"));
-//
-//        // verify that the responseEntity contains the correct data
-//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertEquals(newGuest, responseEntity.getBody());
-//    }
 
 
-    @Test
-    public void testAddGuest() {
-        GuestRequestDTO guestRequestDTO = new GuestRequestDTO();
-        guestRequestDTO.setGuestName("John Doe");
-        guestRequestDTO.setGuestEmail("johndoe@example.com");
-       // guestRequestDTO.setThumbnail("thumbnail.jpg");
 
-        Guest guest = new Guest();
-        guest.setGuestName("John Doe");
-        guest.setGuestEmail("johndoe@example.com");
-        guest.setThumbnail("thumbnail.jpg");
 
-        when(guestService.save(any(Guest.class), any(org.springframework.web.multipart.MultipartFile.class))).thenReturn(guest);
-
-        ResponseEntity<Guest> response = guestController.addGuest(guestRequestDTO);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("John Doe", response.getBody().getGuestName());
-        assertEquals("johndoe@example.com", response.getBody().getGuestEmail());
-        assertEquals("thumbnail.jpg", response.getBody().getThumbnail());
-
-        verify(guestService, times(1)).save(any(Guest.class), any(org.springframework.web.multipart.MultipartFile.class));
-    }
-
-//    @Test
-//    public void testAddGuest() throws Exception {
-//        // Mock the guest request DTO
-//        GuestRequestDTO guestRequestDTO = new GuestRequestDTO();
-//        guestRequestDTO.setGuestName("John Doe");
-//        //guestRequestDTO.setThumbnail(new byte[] {0x01, 0x02, 0x03});
-//
-//        // Mock the guest entity returned by the guest service
-//        Guest guestEntity = new Guest();
-//        guestEntity.setGuestId(1L);
-//        guestEntity.setGuestName("John Doe");
-//        //guestEntity.setThumbnail(new byte[] {0x01, 0x02, 0x03});
-//
-//        // Mock the guest service's save() method to return the guest entity
-//        when(guestService.save()).thenReturn(guestEntity);
-//
-//        // Call the addGuest() method and check the response
-//        ResponseEntity<Guest> response = guestController.addGuest(guestRequestDTO);
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(guestEntity, response.getBody());
-//
-//        // Verify that the guest service's save() method was called with the correct arguments
-//        verify(guestService).save(argThat(guest -> guest.getGuestName().equals("John Doe")), eq(new byte[] {0x01, 0x02, 0x03}));
-//    }
 }
