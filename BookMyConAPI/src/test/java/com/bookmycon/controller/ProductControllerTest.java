840de50368bd77ceec0dcc57eade341e9dbddb0d
ameyd96 <erameydeshpande@gmail.com>
2023-04-17 13:33:00 +0530

    Ticket Id : 9AHqlS0Z : Junit Test cases
    ## What : Add junit test cases to product controller layer
    
    ## Why : As per ticket description  Added junit test cases to product controller layer

--- BookMyConAPI/src/test/java/com/bookmycon/controller/ProductControllerTest.java
+++ BookMyConAPI/src/test/java/com/bookmycon/controller/ProductControllerTest.java
@@ -1,6 +1,8 @@
 package com.bookmycon.controller;
 
+import com.bookmycon.dto.StockDTO;
 import com.bookmycon.model.Product;
+import com.bookmycon.repository.ProductRepository;
 import com.bookmycon.service.ProductService;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -20,7 +23,8 @@ import static org.mockito.Mockito.*;
 public class ProductControllerTest {
     @Mock
     ProductService productService;
-
+    @Mock
+    private ProductRepository productRepository;
     @InjectMocks
     ProductController productController;
 
@@ -50,4 +54,47 @@ public class ProductControllerTest {
         assertEquals(productList, actual.getBody());
     }
 
+@Test
+public void testAddNewProduct() {
+    Product product = new Product();
+    product.setProductId(123);
+    product.setProductName("Test Product");
+    product.setProductPrice(10);
+
+    when(productService.addProduct(product)).thenReturn(product);
+
+    ResponseEntity<Product> responseEntity = productController.addNewProduct(product);
+
+    assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    assertEquals(product, responseEntity.getBody());
+    verify(productService, times(1)).addProduct(product);
+}
+
+    @Test
+    public void testDeleteProduct() {
+        int productId = 123;
+
+        when(productService.deleteProduct(productId)).thenReturn("Product is deleted..");
+
+        ResponseEntity<String> responseEntity = productController.deleteProduct(productId);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals("Product is deleted..", responseEntity.getBody());
+        verify(productService, times(1)).deleteProduct(productId);
+    }
+
+    @Test
+    public void testUpdateStockOfProduct() {
+        StockDTO stockDTO = new StockDTO();
+        stockDTO.setProductId(123);
+        stockDTO.setStockValue(10);
+
+        when(productService.addStock(stockDTO)).thenReturn("Stock is Updated For product : Test Product");
+
+        ResponseEntity<String> responseEntity = productController.updateStockOfProduct(stockDTO);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals("Stock is Updated For product : Test Product", responseEntity.getBody());
+        verify(productService, times(1)).addStock(stockDTO);
+    }
 }
